b4fb4fc44defa81ea9218b6bf17386909b563842
SamVG <uladimir97@yandex.by>
2025-11-25 21:54:44 +0300

    Lecture 3 90% workers lost mani problem for entring and optimezation and dog string

--- lecture_3/program_about_student_info.py
+++ lecture_3/program_about_student_info.py
@@ -4,10 +4,10 @@ class Student_list:
         self.set_of_students_name = []
 
     @classmethod
-    def check_of_number(self,number: int):
+    def check_of_number(self, number: int):
         ''' This program check true is number or not'''
         while True:
-            if isinstance(number,str) and number=='stop':
+            if isinstance(number, str) and number == 'stop':
                 return 'stop'
             try:
                 return int(number)
@@ -15,7 +15,7 @@ class Student_list:
                 number = input('Enter number please ')
 
     @classmethod
-    def check_of_two_words(self,string: str):
+    def check_of_two_words(self, string: str):
         ''' This program check true is string (two words) or not (for name of students)'''
 
         while True:
@@ -28,7 +28,7 @@ class Student_list:
         new_students = self.check_of_two_words(input('Enter ferst and second name of student '))
         grade = self.check_of_number(input('Enter one grade or enter stop '))
         list_of_number = []
-        while isinstance(grade,int):
+        while isinstance(grade, int):
             list_of_number.append(grade)
 
             grade = self.check_of_number(input('Enter one grade or enter stop '))
@@ -37,31 +37,71 @@ class Student_list:
         self.set_of_students_name.append(new_students)
 
     def add_extra_grape_students(self, name_student):
-        self.check_of_two_words(name_student)
+        name_student=self.check_of_two_words(name_student)
         if name_student in self.set_of_students_name:
             grade = self.check_of_number(input('Enter one grade or enter stop '))
             list_of_number = []
-            while grade.lower() != 'stop':
+            while isinstance(grade, (int, float)):
                 list_of_number.append(grade)
 
                 grade = self.check_of_number(input('Enter one grade or enter stop '))
 
             for i in self.list_of_dict_about_students:
-                if i['name']==name_student:
-                    i['grapes'].update(grade)
+                if i['name'] == name_student:
+                    i['grapes'].extend(list_of_number)
         else:
             print('This student not in list ')
 
     def showe_Student_and_his_grapes_all(self):
 
+        if self.list_of_dict_about_students == []:
+            print(f'Not students ')
         for i in self.list_of_dict_about_students:
-            try:
-                print(f'* {i.keys()} - {sum(i.values()) / len(i.values())}')
-            except ZeroDivisionError:
-                print(f'* {i.keys()} - None')
+
+            if i['grapes'] == []:
+                print(f'{i["name"]} - N/A')
+            else:
+                print(
+                    f"{i['name']} - {round(sum(i['grapes']) / len(i['grapes']), 2)} - this is average raiting this student ")
 
     def max_grabe_students(self):
-        print(list(map(lambda i:{i['name']:sum(i.values()) / len(i.values())}, self.list_of_dict_about_students)))
+        if self.list_of_dict_about_students == []:
+            return(f'Not students ')
+        else:
+            all_grapers = []
+            for i in self.list_of_dict_about_students:
+                if len(i['grapes'])!=0:
+                    all_grapers.extend(i['grapes'])
+        print(f'Average grape for student groop {round(sum(all_grapers) / len(all_grapers), 2)}')
+
+        new_dict_for_stydent = [
+            {
+                'name': x['name'],
+                'average': round(sum(x['grapes']) / len(x['grapes']), 2)
+            }
+            for x in self.list_of_dict_about_students
+            if len(x['grapes']) > 0
+        ]
+
+        best = max(new_dict_for_stydent, key=lambda x: x['average'])
+        worst = min(new_dict_for_stydent, key=lambda x: x['average'])
+
+        print(f"Best  - {best['name']}  - average {best['average']}")
+        print(f"Worst - {worst['name']} - average {worst['average']}")
+        return ('----')
+
+
+
+
+
+def check_of_number(number: int):
+    ''' This program check true is number or not'''
+    while True:
+
+        try:
+            return int(number) and 1 <= int(number) <= 5
+        except ValueError:
+            number = input('Enter number please ')
 
 
 class menu:
@@ -87,22 +127,23 @@ print('Hello this program about student ')
 print('Work menu ')
 work1=menu()
 work1.shove_menu()
-choise_user=int(input('Enter number 1-5 Your choise '))
+choise_user=check_of_number(input('Enter number 1-5 Your choise '))
 while choise_user!=5:
+    corect_choise=[1,2,3,4,5]
     if choise_user==1:
         work1.list_st.add_new_student_in_list_of_students()
+        print('---')
 
     if choise_user==2:
         work1.list_st.add_extra_grape_students(input('Enter name student how you wont add grapes '))
-
+        print('---')
     if choise_user==3:
         work1.list_st.showe_Student_and_his_grapes_all()
-
+        print('---')
     if choise_user==4:
-        work1.list_st.showe_Student_and_his_grapes_all()
+        print(work1.list_st.max_grabe_students())
+        print('---')
+
     work1.shove_menu()
     choise_user = int(input('Enter number 1-5 Your choise '))
 print('Good bay naise to meet you :) ')
-
-
-
